cb6b1ede864a404f3fe4bc2ca5af8672951e32b4
nyaw <nyaw@lepi>
2022-12-04 15:51:54 +0700

    Add Siswa model Add Siswa DAO

---
+++ src/dao/SiswaDao.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import java.util.List;
+import models.Siswa;
+
+/**
+ *
+ * @author nyaw
+ */
+public interface SiswaDao {
+
+    public boolean insert(Siswa s);
+
+    public boolean update(Siswa s);
+
+    public boolean delete(String nim);
+
+    public Siswa getSiswaByNim(String nim);
+
+    public List<Siswa> getAllSiswa();
+
+}
---
+++ src/models/Siswa.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models;
+
+/**
+ *
+ * @author nyaw
+ */
+public class Siswa {
+
+    private int nim;
+
+    public int getNim() {
+        return nim;
+    }
+
+    public void setNim(int nim) {
+        this.nim = nim;
+    }
+
+}
