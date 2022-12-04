9e828ae1c5afb0ac21bff1b6329b2c618fd5c7a9
nyaw <nyaw@lepi>
2022-12-04 15:36:13 +0700

    Add database connection

---
+++ src/utils/DBConnection.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utils;
+
+import java.sql.*;
+
+public class DBConnection {
+
+    public static Connection connection;
+
+    public static Connection getInstance() {
+        if (connection == null) {
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                connection = DriverManager.getConnection(
+                        "jdbc:mysql://phpmyadmin.test/pbo_uas", "root", "fakyu"
+                );
+                System.out.println("");
+            } catch (ClassNotFoundException | SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return connection;
+    }
+}
